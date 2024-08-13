8b8bae6d38b22125e017d0013f1616c92502df48
Jason Brezenski <jasonbrezenski@@yahoo.com>
2024-08-13 14:38:03 -0500

    Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,41 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            SchoolSubject();
         }
+
+        public static void SchoolSubject()
+        {
+            Console.WriteLine("What is your favorite school subject?");
+            string favoriteSubject = Console.ReadLine();
+
+            switch (favoriteSubject.ToLower())
+            {
+                case "math":
+                    Console.WriteLine("Math is cool because it's all about solving problems!");
+                    break;
+
+                case "science":
+                    Console.WriteLine("Science is all around us in real-world applications.");
+                    break;
+
+                case "art":
+                    Console.WriteLine("Art is a fun class to do!");
+                    break;
+
+                case "social studies":
+                    Console.WriteLine("Social Studies is great if you like history.");
+                    break;
+
+                case "accounting":
+                    Console.WriteLine("Accounting is my favorite subject also!");
+                    break;
+
+                default:
+                    Console.WriteLine("Interesting choice! What do you like about it ?");
+                    break;
+            }
+        }
+
     }
 }
